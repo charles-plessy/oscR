52093813d9492d588022dc1fad33a01cc1f8bf30
Charles Plessy <plessy@riken.jp>
2012-01-16 21:14:18 +0900

    Reads an OSCTable and renames its rows, assuming it is in the genomic_coordinate name space.

--- R/read.osc.R
+++ R/read.osc.R
@@ -1,2 +1,23 @@
-read.osc <-
-function (...) {read.table(...)}
+read.osc <- function (file, ...) {
+
+osctable <- read.table(file, header=TRUE, ...)
+
+# Inspect the metadata
+#filehandle <- file(file)
+#open(filehandle)
+#metadata         <- readLines(filehandle, n=1)
+#metadata.name    <- gsub("##(.+) +?= +?(.+)", "\\1", metadata, perl=TRUE)
+#metadata.content <- gsub("##(.+) +?= +?(.+)", "\\2", metadata, perl=TRUE)
+#close(filehandle)
+#return(x)
+
+LocationToName <- function(osctable) {
+# Need to check that the columns are really there
+  with(osctable,
+    paste(chrom, ":", start.0base, "-", end, strand ,sep='')
+  )
+}
+
+rownames(osctable) <- LocationToName(osctable)
+return(osctable)
+}
