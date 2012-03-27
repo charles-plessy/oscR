a1335343d895e8bb81d707ef7da7a287e14ea77f
Charles Plessy <plessy@riken.jp>
2012-03-27 21:04:23 +0900

    Transfer comments between functions.

--- R/read.osc.R
+++ R/read.osc.R
@@ -2,15 +2,6 @@ read.osc <- function (file, drop.norm=FALSE, drop.id=FALSE, ...) {
 
 osctable <- read.table(file, header=TRUE, ...)
 
-# Inspect the metadata
-#filehandle <- file(file)
-#open(filehandle)
-#metadata         <- readLines(filehandle, n=1)
-#metadata.name    <- gsub("##(.+) +?= +?(.+)", "\\1", metadata, perl=TRUE)
-#metadata.content <- gsub("##(.+) +?= +?(.+)", "\\2", metadata, perl=TRUE)
-#close(filehandle)
-#return(x)
-
 LocationToName <- function(osctable) {
 
   if ( osc.namespace(osctable) != "genomic_coordinate")
--- R/read.oscheader.R
+++ R/read.oscheader.R
@@ -1,5 +1,14 @@
 read.oscheader <- function (file, ...) {
 
+# Inspect the metadata
+#filehandle <- file(file)
+#open(filehandle)
+#metadata         <- readLines(filehandle, n=1)
+#metadata.name    <- gsub("##(.+) +?= +?(.+)", "\\1", metadata, perl=TRUE)
+#metadata.content <- gsub("##(.+) +?= +?(.+)", "\\2", metadata, perl=TRUE)
+#close(filehandle)
+#return(x)
+
 n <- 1
 oscheader <- '#'
 
