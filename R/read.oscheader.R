8b676c8e5bed5c14c0b4b38ef6004ecac9030f2d
Charles Plessy <plessy@riken.jp>
2012-03-27 16:59:28 +0900

    read.oscheader: function to read a OSC header without reading the whole file.

---
+++ R/read.oscheader.R
@@ -0,0 +1,12 @@
+scan.osc <- function (file, ...) {
+
+n <- 1
+oscheader <- '#'
+
+while (substr(oscheader[n] ,1 ,1) == '#') {
+  n <- n + 1
+  oscheader <- readLines(file, n=n)
+}
+
+return(oscheader)
+}
