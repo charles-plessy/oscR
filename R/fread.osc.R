d8aa15cf4f812cd68c89a10443becf6e079727f0
Charles Plessy <plessy@riken.jp>
2015-04-23 11:27:45 +0900

    New function fread.osc to read an OSCTable in a data.table.

---
+++ R/fread.osc.R
@@ -0,0 +1,26 @@
+fread.osc <- function (file, dropIdCoords=FALSE, replaceIdCoords=TRUE, drop.norm=TRUE) {
+
+oscHeaderLength <- length(read.oscheader(file)) -1
+
+if (grepl('.gz$', file))
+  file <- paste('zcat', file)
+
+osctable <- fread( file
+                 , skip = oscHeaderLength )
+
+if (drop.norm)
+  osctable <- osctable[, -grep("norm\\.", colnames(osctable)), with=FALSE]
+
+coords <- c("chrom", "start.0base", "end", "strand")
+idCoords <- c("id", coords)
+
+if (dropIdCoords)
+  osctable <- osctable[, setdiff(colnames(osctable), idCoords), with=FALSE] 
+
+if (replaceIdCoords) {
+  osctable$id <- ChromosomeCoordinates(osctable[, idCoords, with=FALSE])
+  osctable <- osctable[, setdiff(colnames(osctable), coords), with=FALSE]
+}
+
+return(osctable)
+}
